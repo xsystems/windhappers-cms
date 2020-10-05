9fc7727b7808b6f16d5b886340c13f3f966a17ca
Koen Boes <kboes@qualogy.com>
2020-10-05 21:48:33 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a14a1820826fac0520ac91bdb9bb272f'),
+    },
+  },
+});
