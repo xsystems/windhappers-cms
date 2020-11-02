b7a3076051f62051f3c2f35b7b10c09a826a7484
Koen Boes <kboes@qualogy.com>
2020-11-02 22:17:18 +0100

    Update configuration environment variables

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('CMS_DATABASE_HOST', 'postgres'),
-        port: env.int('CMS_DATABASE_PORT', 5432),
-        database: env('CMS_DATABASE_NAME', 'windhappers-cms'),
-        username: env('CMS_DATABASE_USERNAME', 'postgres'),
-        password: env('CMS_DATABASE_PASSWORD', 'postgres'),
-        ssl: env.bool('CMS_DATABASE_SSL', false),
+        host: env('CMS_API_DATABASE_HOST', 'postgres'),
+        port: env.int('CMS_API_DATABASE_PORT', 5432),
+        database: env('CMS_API_DATABASE_NAME', 'windhappers-cms'),
+        username: env('CMS_API_DATABASE_USERNAME', 'postgres'),
+        password: env('CMS_API_DATABASE_PASSWORD', 'postgres'),
+        ssl: env.bool('CMS_API_DATABASE_SSL', false),
       },
       options: {}
     },
